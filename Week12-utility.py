55361625dbc465f6e604b60dede455d6e53aebdb
Hannah Taylor <hannahmtaylor19@gmail.com>
2019-11-19 08:24:41 -0700

    Replaces one index of a string with another

--- Week12-utility.py
+++ Week12-utility.py
@@ -5,6 +5,12 @@ def LoadFile(a):
     f=open(a,'r+')
     contents=f.read()
     f.close()
-    print('OUTPUT ' + content)
+    PrintOutput(contents)
+
+def UpdateString(a,b,c):
+    string=a
+    string[c]=string.replace((string[c]),b,1)
+    print(string)
+    
     
     
