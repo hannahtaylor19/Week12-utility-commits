e1453c6d85096a31a1f9eb7ffa3dd8ad1065cdb8
Hannah Taylor <hannahmtaylor19@gmail.com>
2019-11-21 15:16:52 -0700

    Final Code for all functions

--- Week12-utility.py
+++ Week12-utility.py
@@ -1,3 +1,7 @@
+#
+# Hannah Taylor
+#CSCI 102 - Scetion B
+#Week 11 - Part B 
 def PrintOutput(a):
     print('OUTPUT ' + str(a))
     
@@ -5,21 +9,57 @@ def LoadFile(a):
     f=open(a,'r+')
     contents=f.read()
     f.close()
-    PrintOutput(contents)
+    contents=contents.split()
+    return contents
 
 def UpdateString(a,b,c):
     string=a
-    string[c]=string.replace((string[c]),b,1)
-    print(string)
-def FindWordCount(a,b):
+    string_list=[]
+    updated_string=''
+    for char in string:
+        string_list+= char
+    string_list[c]=b
+    updated_string=updated_string.join(string_list)
+    PrintOutput(updated_string)
 
-def ScoreFinder(a,b,c):
+def FindWordCount(a,b):
+    count=0
+    i=0
+    while i < (len(a)):
+        if a[i] == b:
+            count+=1
+        i+=1
+    return count
+
+def ScoreFinder(players,scores,a):
+    a=a.capitalize()
+    if a in players:
+        player_index=players.index(a)
+        players_score=scores[(int(player_index))]
+        PrintOutput(( str(a) + ' got a score of ' + str(players_score)))
+    else:
+        PrintOutput('player not found') 
+
 
 def Union(a,b):
+    union=[]
+    union=a+b
+    return str(union)
 
-def Intersection(a,b)
+def Intersection(a,b):
+    intersection=[]
+    i=0
+    while i < (len(a)):
+        if a[i] in b:
+            intersection.append(str(a[i]))
+        i += 1
+    return intersection 
 
 def NotIn(a,b):
-    
-    
-    
+    Not_In=[]
+    i=0
+    while i < (len(a)):
+        if a[i] not in b:
+            Not_In.append(str(a[i]))
+        i += 1
+    return Not_In 
